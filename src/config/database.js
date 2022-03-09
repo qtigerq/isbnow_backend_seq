177ddafdfaa289fc79e971c0c997fe88895c4df5
Tiago <chicobg@gmail.com>
2022-03-08 22:31:47 -0300

    ajuste dos dados do BD

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
     dialect: 'mysql',
     host: 'localhost',
-    username: 'root',
-    password: 'docker',
-    database: 'isbnow',
+    username: 'tiagomelo',
+    password: 'pipoca123',
+    database: 'tiagomelo',
     define: {
         timestamps: true,       //Usa os campos created_at e updated_at
         underscored: true,      //Nome no formato Snake Case. Ex: user_group
