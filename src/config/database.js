92d31e84182bc03395e723f7624fa9f64998041b
Tiago <chicobg@gmail.com>
2022-03-13 20:00:48 -0300

    Config BD

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
+    host: 'mysql.tiagomelo.kinghost.net',
     username: 'tiagomelo',
     password: 'pipoca123',
     database: 'tiagomelo',
